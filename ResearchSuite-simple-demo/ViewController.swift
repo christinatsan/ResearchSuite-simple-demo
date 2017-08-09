06951f74b685994be66f4ac7926ee0c3a9030676
christinatsan <ctsango000@citymail.cuny.edu>
2017-08-09 17:04:28 -0400

    Initial Commit

---
+++ ResearchSuite-simple-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResearchSuite-simple-demo
+//
+//  Created by Christina Tsangouri on 8/9/17.
+//  Copyright © 2017 Christina Tsangouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
